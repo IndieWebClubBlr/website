970e3c152b733444b6361283fa1a82fa40917207
Abhinav Sarkar <abhinavsarkar@google.com>
2025-10-05 14:18:49 +0530

    Reduces blogroll post time to six months and count to one

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from zoneinfo import ZoneInfo
 # Configuration constants
 REQUEST_TIMEOUT = 30  # seconds
 MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5MB
-MAX_SHOWN_ENTRIES = 2
+MAX_SHOWN_ENTRIES = 1
 MAX_FEED_ENTRIES = 10
 MAX_SHOWN_TAGS = 5
 MAX_WORKERS = 10  # concurrent feed fetches
-RECENT_DAYS = 365  # one year
+RECENT_DAYS = 182  # half year
 UA = "IndieWebClub BLR website generator"
 SITE_URL = "https://indiewebclubblr.github.io/website/"
 WEBCAL_URL = SITE_URL.replace("https", "webcal")
