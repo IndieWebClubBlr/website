da547e4b8b9830c7ead683641bd0c3049122b9f6
Abhinav Sarkar <abhinavsarkar@google.com>
2026-04-03 10:31:05 +0530

    Increases feed entry age

--- src/config.py
+++ src/config.py
@@ -16,7 +16,7 @@ UA = "blr.indiewebclub.org generator"
 MAX_CONTENT_LENGTH = 10 * 1024 * 1024
 
 # Maximum age in days of recent entries to fetch from each feed.
-MAX_FEED_ENTRY_AGE = 150
+MAX_FEED_ENTRY_AGE = 180
 
 # Maximum number of recent entries to fetch from each feed.
 MAX_FEED_ENTRIES = 100
